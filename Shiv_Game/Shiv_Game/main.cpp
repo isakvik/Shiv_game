2d0df47e5046e168411da30e98758f5d1aadc810
isakvik <hjeisa14@gmail.com>
2016-05-13 17:50:53 +0200

    testing parameters

--- Shiv_Game/Shiv_Game/main.cpp
+++ Shiv_Game/Shiv_Game/main.cpp
@@ -13,11 +13,14 @@ int main( int argc, char* args[] ){
 
 	cout << obj_1.fGFX_initialize() << endl;
 	
-	SDL_Rect source = {0,0,228,228};
+	SDL_Rect source = {0,0,45,90};
+	SDL_Rect source2 = {45,0,45,90};
 	SDL_Rect dest = {100,100,100,100};
+	SDL_Rect dest2 = {200,100,100,100};
 
 	SDL_RenderClear( obj_1.getRenderer() );
 	SDL_RenderCopy( obj_1.getRenderer() , obj_1.fGFX_loadTexture("test.png"), &source, &dest);
+	SDL_RenderCopy( obj_1.getRenderer() , obj_1.fGFX_loadTexture("test.png"), &source2, &dest2);
 	SDL_RenderPresent( obj_1.getRenderer() );
 
 	//While application is running
