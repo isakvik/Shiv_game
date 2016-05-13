3dbbd8841aa56509da50a5df78157a4de3cc11d9
isakvik <hjeisa14@gmail.com>
2016-05-13 15:59:31 +0200

    oppdatert mainmetode til å funke med gfx test

--- Shiv_Game/Shiv_Game/main.cpp
+++ Shiv_Game/Shiv_Game/main.cpp
@@ -1,38 +1,40 @@
 #include "framework.h"
+#include "frameworkGraphics.h"
 #include <iostream>
-#include "frameworkSound.h"
-#include "SDL.h"
 
 using namespace std;
 
+SDL_Event e;
+bool quit = false;
+
 int main( int argc, char* args[] ){
+	
+	frameworkGraphics obj_1;
 
-    cout << frameworkInit() << endl;
+	cout << obj_1.fGFX_initialize() << endl;
+	
+	SDL_Rect source = {0,0,228,228};
+	SDL_Rect dest = {100,100,100,100};
 
-    bool quit = false;
-    SDL_Event e;
+	SDL_RenderClear( obj_1.getRenderer() );
+	SDL_RenderCopy( obj_1.getRenderer() , obj_1.fGFX_loadTexture("test.png"), &source, &dest);
+	SDL_RenderPresent( obj_1.getRenderer() );
 
-	frameworkSound obj_1;
-
-	obj_1.loadSound("D:\\GitHub\\Shiv_game\\Shiv_Game\\Shiv_Game\\test.wav");
-	obj_1.playSound();
-    //While application is running
+	//While application is running
     while( !quit )
-    {
-        while( SDL_PollEvent( &e ) != 0 )
-        {	
-			
-			
-            //User requests quit
-            if( e.type == SDL_QUIT )
-            {
-                quit = true;
-            }
-        }
-    }
+	{
+		while( SDL_PollEvent( &e ) != 0 )
+		{
+			//User requests quit
+			if( e.type == SDL_QUIT )
+			{
+				quit = true;
+			}
+		}
+		
+	}
 
-    frameworkQuit();
+	obj_1.fGFX_quit();
 
-    return 0;
+	return 0;
 }
-
